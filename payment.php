89a93a024322daebf97cd4ac9670edb42f34d431
unknown <ziko.nh2002@gmail.com>
2023-01-17 17:49:21 +0100

    first commit

---
+++ payment.php
@@ -0,0 +1,86 @@
+<?php 
+session_start();
+$title = "Payment";
+require "./template/header.php";
+?>
+<title>Stripe Payment Gateway Integration in PHP</title>
+<script type="text/javascript" src="https://js.stripe.com/v2/"></script>
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery-creditcardvalidator/1.0.0/jquery.creditCardValidator.js"></script>
+<script type="text/javascript" src="script/payment.js"></script>
+
+<div class="container">	
+	<div class="row">	
+		<h2>last step before you own your product  </h2>	
+
+		<?php 
+		if(isset($_SESSION["message"]) && $_SESSION["message"] && $_SESSION["message"] == 'failed') {
+		?>			
+			<div class="alert alert-danger">
+			  <?php 
+			  echo "Error : Payment failed!"; 
+			  $_SESSION["message"] = '';
+			  ?>
+			</div>
+		<?php 
+		} elseif(isset($_SESSION["message"]) && $_SESSION["message"]) {
+		?>
+			<div class="alert alert-success">
+			  <?php 
+			  echo $_SESSION["message"]; 
+			  $_SESSION["message"] = '';
+			  ?>
+			</div>
+		<?php } ?>
+		<div class="panel panel-default">	
+		<form method="post" action="process.php" class="form-horizontal">		
+			<div class="panel-heading">Order Process</div>
+			<div class="panel-body">
+				<form action="process.php" method="POST" id="paymentForm">	
+					<div class="row">
+						<div class="col-md-12" style="border-right:1px solid #ddd;">
+							<hr>
+							<h4 align="center">Payment Details</h4>
+							<div class="form-group">
+								<label>Card holder <span class="text-danger">*</span></label>
+								<input type="text" name="cardNumber" id="cardNumber" class="form-control" placeholder="Full Name" maxlength="20" onkeypress="">
+								<span id="errorCardNumber" class="text-danger"></span>
+							</div>
+							<div class="form-group">
+								<label>Card Number <span class="text-danger">*</span></label>
+								<input type="text" name="cardNumber" id="cardNumber" class="form-control" placeholder="1234 5678 9012 3456" maxlength="20" onkeypress="">
+								<span id="errorCardNumber" class="text-danger"></span>
+							</div>
+							<div class="form-group">
+								<div class="row">
+									<div class="col-md-4">
+										<label>Expiry Month</label>
+										<input type="text" name="cardExpMonth" id="cardExpMonth" class="form-control" placeholder="MM" maxlength="2" onkeypress="return validateNumber(event);">
+										<span id="errorCardExpMonth" class="text-danger"></span>
+									</div>
+									<div class="col-md-4">
+										<label>Expiry Year</label>
+										<input type="text" name="cardExpYear" id="cardExpYear" class="form-control" placeholder="YYYY" maxlength="4" onkeypress="return validateNumber(event);">
+										<span id="errorCardExpYear" class="text-danger"></span>
+									</div>
+									<div class="col-md-4">
+										<label>CVC</label>
+										<input type="text" name="cardCVC" id="cardCVC" class="form-control" placeholder="123" maxlength="4" onkeypress="return validateNumber(event);">
+										<span id="errorCardCvc" class="text-danger"></span>
+									</div>
+								</div>
+							</div>
+							<br>
+							<div align="center">
+							<button type="submit" class="btn btn-primary" >buy</button>
+							</div>
+							<br>
+						</div>
+						
+					</div>
+				</form>
+			</div>
+		</div>			
+	</div>		
+</div>
+<?php require_once "./template/footer.php";?>
+
